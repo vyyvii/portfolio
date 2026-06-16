0bc461266eae9292600f9ae0dd978f0982b36bfb
Victor Defauchy <victor.defauchy@epitech.eu>
2026-06-16 16:51:39 +0200

    FIX: name

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "Victor Defauchy",
   description: "Portfolio of Victor Defauchy",
 };
 
